679ce13aa6c0f4cc02922aba36b4ee425a7a3f98
Rob Godfrey <rob_godfrey@outlook.com>
2023-10-08 21:40:23 -1000

    Move closet/store grids back to their respective parents

--- lib/screens/closet.dart
+++ lib/screens/closet.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../components/closet/closet_grid.dart';
-
 class ClosetScreen extends StatefulWidget {
   const ClosetScreen({super.key});
 
@@ -20,13 +18,13 @@ class _ClosetState extends State<ClosetScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       resizeToAvoidBottomInset: true,
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 40),
-            Text(
+            const SizedBox(height: 40),
+            const Text(
               'Closet',
               style: TextStyle(
                 fontSize: 24,
@@ -34,8 +32,70 @@ class _ClosetState extends State<ClosetScreen> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(left: 26, right: 26),
-              child: ClosetGrid(),
+              padding: const EdgeInsets.only(left: 26, right: 26),
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 1,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 0.8,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                              color: Colors.black,
+                              width: 2
+                          ),
+                        ),
+                        child: const Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Image(
+                                image: AssetImage('assets/images/accessories/hat-cowboy.png'),
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                            Text(
+                              'Cowboy Hat',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'EQUIP',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
             ),
           ],
         ),
--- lib/screens/store.dart
+++ lib/screens/store.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:spark_pet/components/closet/store_grid.dart';
+
+import '../icons/bone_icon.dart';
 
 class StoreScreen extends StatefulWidget {
   const StoreScreen({super.key});
@@ -12,13 +13,13 @@ class _StoreState extends State<StoreScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       resizeToAvoidBottomInset: true,
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 40),
-            Text(
+            const SizedBox(height: 40),
+            const Text(
               'Store',
               style: TextStyle(
                 fontSize: 24,
@@ -26,8 +27,83 @@ class _StoreState extends State<StoreScreen> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(left: 26, right: 26),
-              child: StoreGrid(),
+              padding: const EdgeInsets.only(left: 26, right: 26),
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 9,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 0.8,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                              color: Colors.black,
+                              width: 2
+                          ),
+                        ),
+                        child: const Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Image(
+                                image: AssetImage('assets/images/accessories/hat-cowboy.png'),
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                            Text(
+                              'Cowboy Hat',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Center(
+                                child: Text(
+                                  '200',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              Center(
+                                child: Icon(
+                                  BoneIcon.bone,
+                                  size: 20,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
             ),
           ],
         ),
