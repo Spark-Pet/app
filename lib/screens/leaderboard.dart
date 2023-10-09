a0162a68a62592646621fba8e39877be60dd841d
Rob Godfrey <rob_godfrey@outlook.com>
2023-10-08 16:38:41 -1000

    Make leaderboard clickable

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -48,90 +48,110 @@ class LeaderboardScreen extends StatefulWidget {
 
 class _LeaderboardState extends State<LeaderboardScreen> {
 
+  TableRow clickableTableRow({
+    required List<Widget> children,
+    required void Function() onTap,
+  }) {
+    return TableRow(
+      children: children.map((child) {
+        return GestureDetector(
+          onTap: onTap,
+          child: child,
+        );
+      }).toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Daily Leaderboard',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      body: Column(
+        children: <Widget>[
+          const SizedBox(height: 50),
+          const Text(
+            'Daily Leaderboard',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            'Thursday, Oct 5th'
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+            child: Table(
+              columnWidths: const {
+                0: FlexColumnWidth(1),
+                1: FlexColumnWidth(4),
+                2: FlexColumnWidth(4),
+              },
+              border: TableBorder.all(
+                color: Colors.grey,
+                width: 1,
               ),
-            ),
-            const Text(
-              'Thursday, Oct 5th'
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-              child: Table(
-                columnWidths: const {
-                  0: FlexColumnWidth(1),
-                  1: FlexColumnWidth(4),
-                  2: FlexColumnWidth(4),
-                },
-                border: TableBorder.all(
-                  color: Colors.grey,
-                  width: 1,
+              children: [
+                const TableRow(
+                  children: [
+                    SixteenBoldVCentered(''),
+                    SixteenBoldVCentered('Username'),
+                    SixteenBoldVCentered('Steps'),
+                  ],
                 ),
-                children: const [
-                  TableRow(
-                    children: [
-                      SizedBox(),
-                      SixteenBoldVCentered('Name'),
-                      SixteenBoldVCentered('Total Steps'),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      SixteenBoldVCentered('1'),
-                      SixteenVCentered('username1'),
-                      SixteenVCentered('24,012'),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      SixteenBoldVCentered('2'),
-                      SixteenVCentered('username2'),
-                      SixteenVCentered('23,963'),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      SixteenBoldVCentered('3'),
-                      SixteenVCentered('username3'),
-                      SixteenVCentered('22,146'),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      SixteenBoldVCentered('4'),
-                      SixteenVCentered('username4'),
-                      SixteenVCentered('16,754'),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      SixteenBoldVCentered('5'),
-                      SixteenVCentered('username5'),
-                      SixteenVCentered('15,243'),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      SixteenBoldVCentered('6'),
-                      SixteenVCentered('username6'),
-                      SixteenVCentered('14,599'),
-                    ],
-                  ),
-                ],
-              ),
+                clickableTableRow(
+                  children: [
+                    SixteenBoldVCentered('1'),
+                    SixteenVCentered('username1'),
+                    SixteenVCentered('24,012'),
+                  ],
+                  onTap: () {
+                    print('Row 1 clicked');
+                  },
+                ),
+                clickableTableRow(
+                  children: [
+                    SixteenBoldVCentered('2'),
+                    SixteenVCentered('username2'),
+                    SixteenVCentered('23,847'),
+                  ],
+                  onTap: () {
+                    print('Row 2 clicked');
+                  },
+                ),
+                clickableTableRow(
+                  children: [
+                    SixteenBoldVCentered('3'),
+                    SixteenVCentered('username3'),
+                    SixteenVCentered('23,251'),
+                  ],
+                  onTap: () {
+                    print('Row 3 clicked');
+                  },
+                ),
+                clickableTableRow(
+                  children: [
+                    SixteenBoldVCentered('4'),
+                    SixteenVCentered('username4'),
+                    SixteenVCentered('22,616'),
+                  ],
+                  onTap: () {
+                    print('Row 4 clicked');
+                  },
+                ),
+                clickableTableRow(
+                  children: [
+                    SixteenBoldVCentered('5'),
+                    SixteenVCentered('username5'),
+                    SixteenVCentered('21,913'),
+                  ],
+                  onTap: () {
+                    print('Row 5 clicked');
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
