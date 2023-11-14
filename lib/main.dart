7e52891dbb8018fe7c49718c9048a9a1cb84cbf5
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-13 23:08:52 -1000

    Initial firebase auth setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spark_pet/sparkpet.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseUIAuth.configureProviders([EmailAuthProvider()]);
   runApp(const ProviderScope(child: SparkPetApp()));
 }
 
@@ -12,6 +20,36 @@ class SparkPetApp extends StatelessWidget {
   // Root of application
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SparkPet());
+    final auth = FirebaseAuth.instance;
+
+    if (auth.currentUser == null || auth.currentUser!.isAnonymous) {
+      return MaterialApp(
+        title: 'SparkPet',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        home: const Scaffold(
+          body: SafeArea(
+            child: SignInScreen(),
+          ),
+        ),
+      );
+    }
+    if (!auth.currentUser!.emailVerified && auth.currentUser!.email != null) {
+      return MaterialApp(
+        title: 'SparkPet',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        home: const Scaffold(
+          body: SafeArea(
+            child: EmailVerificationScreen(),
+          ),
+        ),
+      );
+    }
+    return const SparkPet();
   }
 }
