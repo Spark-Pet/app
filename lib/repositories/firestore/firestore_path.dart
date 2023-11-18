b852d2713f64dcc48425677b58245443b03a5996
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-17 22:04:20 -1000

    Add firebase

---
+++ lib/repositories/firestore/firestore_path.dart
@@ -0,0 +1,17 @@
+/// Defines the domain model path strings for [FirestoreService].
+class FirestorePath {
+  static String userData(String id) => 'userData/$id';
+  static String allUserData() => 'userData';
+
+  static String userStats(String id) => 'userStats/$id';
+  static String allUsersStats() => 'usersStats';
+
+  static String petStats(String id) => 'petStats/$id';
+  static String allPetsStats() => 'petsStats';
+
+  static String accessoryData(String id) => 'accessoryData/$id';
+  static String allAccessoryData() => 'accessoryData';
+
+  static String challengeData(String id) => 'challengeData/$id';
+  static String allChallengeData() => 'challengeData';
+}
