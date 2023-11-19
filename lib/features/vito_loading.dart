3510a96957b0302ab2303e1e819a79a651640938
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-18 21:18:47 -1000

    Add loading and error

---
+++ lib/features/vito_loading.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VitoLoading extends StatelessWidget {
+  const VitoLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+}
